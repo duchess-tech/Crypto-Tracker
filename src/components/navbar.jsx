876b7b8638709f404e7c86b5c335edfb39d79dfa
esther odedoyin <estherodedoyin1@example.com>
2023-12-07 13:17:08 +0100

    updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,12 +7,12 @@ function Nav() {
             <header className="blogname w-full  xl:w-2/5 p-2 xl:p-0 text-center">
                 <div className="xl:text-2xl text-lg  font-bold">
 
-                    <Link to={"/Home"} className="flex"> <span>Crypt</span><img src="./src/images/bitcoin2.jpg" alt="" className="w-[30px]" /><span>CurrencyTracker</span> </Link>
+                    <Link to={"/Home"} className="flex"> <span>Crypt</span><img src="images/bitcoin2.jpg" alt="" className="w-[30px]" /><span>CurrencyTracker</span> </Link>
                 </div>
             </header>
 
 
-            <div className="flex xl:w-1/5 w-full  xl:justify-between text-black    font-bold  gap-4 mt-3 xl:text-md text-md hover:text-[#ffffff]">
+            <div className="flex xl:w-1/5 w-full  xl:justify-between text-black font-bold  gap-4 mt-3 xl:text-md text-md hover:text-[#ffffff]">
                 <button ><Link>About</Link></button>
                 <button ><Link>Contact</Link></button>
 
